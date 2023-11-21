c4d8229b5fa46aa2370a481a4c1e97a97b8fc84c
Nehan-Shanuka <124347211+Nehan-Shanuka@users.noreply.github.com>
2023-11-21 13:18:22 +0530

    Defining the class User

---
+++ src/com/oop/cw/pase_01/User.java
@@ -0,0 +1,24 @@
+package com.oop.cw.pase_01;
+
+public class User {
+    private String username;
+    private String password;
+
+    // Constructor with given username and password
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    // A public method for retrieving the username
+    public String getUsername() {
+        return username;
+    }
+    // A public method for retrieving the password
+    public String getPassword() {
+        return password;
+    }
+    // A public method for assigning into the password
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
